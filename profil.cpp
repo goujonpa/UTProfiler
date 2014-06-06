55cd172e569073789ee47fbf5d2326580adbbf15
goujonpa <goujonpa@gmail.com>
2014-06-06 15:31:55 +0200

    Up Profil

--- profil.cpp
+++ profil.cpp
@@ -4,3 +4,36 @@ Profil::Profil(QObject *parent) :
     QObject(parent)
 {
 }
+
+
+bool Profil::setInscriptions(QVector<Inscription*> insc)
+{
+    m_inscriptions = insc;
+    return true;
+}
+
+bool Profil::addInscription(Inscription* insc)
+{
+    m_inscriptions.push_back(insc);
+    return true;
+}
+
+bool setActuel(Cursus* actuel)
+{
+
+}
+
+bool setVise(Cursus* vise)
+{
+
+}
+
+bool setEtranger(QVector<Etranger*> etr);
+{
+
+}
+
+bool addEtranger(Etranger* etr)
+{
+
+}
--- profil.h
+++ profil.h
@@ -2,6 +2,7 @@
 #define PROFIL_H
 
 #include <QObject>
+#include <QVector>
 #include "inscription.h"
 #include "cursus.h"
 #include "etranger.h"
@@ -9,14 +10,31 @@
 class Profil : public QObject // sera singleton
 {
         Q_OBJECT
+
     public:
+
+        Profil(unsigned int id, QVector<Inscription*> insc, Cursus* actuel, Cursus* vise, QVector<Etranger*> etr): m_id(id), m_inscriptions(insc), m_actuel(actuel), m_vise(vise), m_etranger(etr) {}
         explicit Profil(QObject *parent = 0);
+        unsigned int getId() const {return m_id;}
+        QVector<Inscription*> getInscriptions() const {return m_inscriptions;}
+        Cursus* getActuel() const {return m_actuel;}
+        Cursus* getVise() const {return m_vise;}
+        QVector<Etranger*> getEtranger() {return m_etranger;}
+        bool setInscriptions(QVector<Inscription*> insc);
+        bool addInscription(Inscription* insc);
+        bool setActuel(Cursus* actuel);
+        bool setVise(Cursus* vise);
+        bool setEtranger(QVector<Etranger*> etr);
+        bool addEtranger(Etranger* etr);
+
+
+
     protected:
-        int m_id;
-        Inscription** m_inscriptions;
+        unsigned int m_id;
+        QVector<Inscription*> m_inscriptions;
         Cursus* m_actuel;
         Cursus* m_vise;
-        Etranger** m_etranger;
+        QVector<Etranger*> m_etranger;
 
     signals:
 
