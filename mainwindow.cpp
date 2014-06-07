420b544db6b14b3bb835edfc7873c75b7288f3cd
goujonpa <goujonpa@gmail.com>
2014-06-07 17:39:43 +0200

    Debut fenetre

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QGridLayout>
+#include <QLabel>
+#include <QPushButton>
+
+
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,11 +17,39 @@ MainWindow::MainWindow(QWidget *parent) :
     QWidget *zoneCentrale = new QWidget;
     setCentralWidget(zoneCentrale);
 
+
+    // ===== Actions & Menus =====
+
     QAction *actionProfil = ui->menuProfil->addAction("Profil");
     QAction *actionSimulation = ui->menuSimulation->addAction("Simulation");
     QAction *actionConfig = ui->menuUTProfiler->addAction("Configuration");
 
-    QToolBar *barreOutil = addToolBar("Profil");
+
+
+
+    // ===== Main Layout =====
+
+    QGridLayout *mainlayout = new QGridLayout;
+    zoneCentrale->setLayout(mainlayout);
+
+    QPushButton *boutonProfil = new QPushButton;
+    boutonProfil->setText("Profil");
+
+    QPushButton *boutonSimulation = new QPushButton;
+    boutonSimulation->setText("Simulation");
+
+    QPushButton *boutonConfiguration = new QPushButton;
+    boutonConfiguration->setText("Configuration");
+
+    mainlayout->addWidget(boutonProfil,0,0,1,1);
+    mainlayout->addWidget(boutonSimulation,0,1,1,1);
+    mainlayout->addWidget(boutonConfiguration,0,2,1,1);
+
+    QLabel *affichageProfil = new QLabel;
+    affichageProfil->setText("Profil");
+    mainlayout->addWidget(affichageProfil, 1,0,10,3);
+
+
 
 
 
